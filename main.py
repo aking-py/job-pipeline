b74ef30816fa76bab2536d96b63222f6f2c60680
Amjad <king.a27@outlook.com>
2026-04-27 01:03:57 +0100

    Added schedular to main.py - full automation complete

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import subprocess
-subprocess.run(["python","src/clean.py"])
-subprocess.run(["python","src/analyse.py"])
+import schedule 
+import time  
+
+def run_pipeline():
+    subprocess.run(["python","src/clean.py"])
+    subprocess.run(["python","src/analyse.py"])
+
+schedule.every().monday.at("09:00").do(run_pipeline)
+
+while True: 
+    schedule.run_pending()
+    time.sleep(3600) 
